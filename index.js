9fd9946a13b26fc23b8dacd6b0f3efb0c8ab2364
István Soós <isoos@users.noreply.github.com>
2016-11-08 11:10:38 +0100

    multi-series collectors (#3)

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ var influx = require('influx');
 var influx_udp = require('influx-udp');
 var url = require('url');
 
-// create a collector for the given series
-function Collector(series, uri) {
+// create a collector for all series
+function Collector(uri) {
     if (!(this instanceof Collector)) {
-        return new Collector(series, uri);
+        return new Collector(uri);
     }
 
     var self = this;
 
     if (!uri) {
         return;
-    }
-
-    if (!series) {
-        throw new Error('series name must be specified');
     }
 
     var parsed = url.parse(uri, true /* parse query args */);
@@ -43,11 +39,10 @@ function Collector(series, uri) {
         reportTime : false
     });
 
-    self._points = [];
+    self._series = {};
 
     var opt = parsed.query || {};
 
-    self._series_name = series;
     self._instant_flush = opt.instantFlush == 'yes';
     self._time_precision = opt.time_precision;
 
@@ -60,7 +55,7 @@ function Collector(series, uri) {
 
     // flush on an interval
     // or option to auto_flush=false
-    setInterval(function() {
+    self._interval = setInterval(function() {
         self.flush();
     }, flush_interval).unref();
 }
@@ -83,50 +78,78 @@ Collector.prototype.computePointCountToSend = function(pointSizes, upperBound) {
   return Math.min(index, pointSizes.length); // But not if there were no points at all.
 };
 
-Collector.prototype.flush = function() {
-    var self = this;
-
-    if (!self._points || self._points.length === 0) {
+function flushSeries(collector, seriesName, points) {
+    if (!points || points.length === 0) {
         return;
     }
 
     // only send N points at a time to avoid making requests too large
-    // TODO what if we are backed up?
+    var spliceIndex;
+    if (collector._client.protocol == 'udp:') {
+      spliceIndex = collector.computePointCountToSend(collector._points.map(function (point) {
+        return JSON.stringify(point).length;
+      }), MTU_SIZE);
+    } else {
+      spliceIndex = 50;
+    }
+    var batch = points.splice(0, spliceIndex);
+    var opt = { precision: collector._time_precision };
 
-    var spliceIndex = self.computePointCountToSend(self._points.map(function (point) {
-      return JSON.stringify(point).length;
-    }), MTU_SIZE);
-    var points = self._points.splice(0, spliceIndex);
-    var opt = { precision: self._time_precision };
-
-    self._client.writePoints(self._series_name, points, opt, function(err) {
+    collector._client.writePoints(seriesName, batch, opt, function(err) {
         if (err) {
             // TODO if error put points back to send again?
             return self.emit('error', err);
         }
 
         // there are more points to flush out
-        if (self._points.length >0) {
-            setImmediate(self.flush.bind(self));
+        if (points.length > 0) {
+            flushSeries(collector, seriesName, points);
         }
     });
+}
+
+Collector.prototype.flush = function() {
+    var self = this;
+
+    Object.keys(self._series).forEach(function(key) {
+        var series = self._series[key];
+        delete self._series[key];
+
+        flushSeries(self, key, series);
+    });
 };
+
+function getSeries(collector, name, reset) {
+    var series = collector._series[name];
+
+    if(!series) {
+        series = [];
+        collector._series[name] = series;
+    }
+
+    return series;
+}
 
 // collect a data point (or object)
 // @param [Object] value the data
 // @param [Object] tags the tags (optional)
-Collector.prototype.collect = function(value, tags) {
+Collector.prototype.collect = function(seriesName, value, tags) {
     var self = this;
 
-    // disabled (due to no URL)
-    if (!self._points) {
-        return;
-    }
-
-    self._points.push([value, tags]);
     if (self._instant_flush) {
-        self.flush();
+        flushSeries(self, seriesName, [[value, tags]]);
+    } else {
+        var series = getSeries(self, seriesName);
+
+        series.push([value, tags]);
     }
 };
+
+Collector.prototype.stop = function() {
+    var self = this;
+
+    clearInterval(self._interval);
+    self.flush();
+}
 
 module.exports = Collector;
